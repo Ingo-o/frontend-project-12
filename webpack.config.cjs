516a1de91c3eede6bff850b0b3802b8495edaba5
IvanZezyukin <i.zezukin@gmail.com>
2022-09-10 22:08:53 +0300

    move output to dist and add dist to ignore

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
